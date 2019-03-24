88e7373efe00a715df55ecdc59b5317a1d8bbab6
QuantumScrewdriver <bsk945@gmail.com>
2019-03-24 15:15:53 -0700

    Updates

---
+++ precache-manifest.6292904707fe1a547e82dbd4525a342d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "778680f9e9724aadf5a2",
+    "url": "/stalking-congress/static/js/runtime~main.af6e5429.js"
+  },
+  {
+    "revision": "60d76806e9da1dcd6cdd",
+    "url": "/stalking-congress/static/js/main.975bfd18.chunk.js"
+  },
+  {
+    "revision": "3878c26bf9f30a0cb038",
+    "url": "/stalking-congress/static/js/2.43a3b2a5.chunk.js"
+  },
+  {
+    "revision": "60d76806e9da1dcd6cdd",
+    "url": "/stalking-congress/static/css/main.57b25021.chunk.css"
+  },
+  {
+    "revision": "9cf4241467022b0f6832818b8d7f3979",
+    "url": "/stalking-congress/index.html"
+  }
+];
